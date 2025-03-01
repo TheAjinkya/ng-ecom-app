f5003c88090fe149931dbe663e88848c7af71bad
Ajinkya Chanshetty <ajinkya.chanshetty@gmail.com>
2025-03-01 23:49:50 +0000

    Created some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductDetailsComponent } from './product/product-details/product-details.component';
+import { CartDetailsComponent } from './cart/cart-details/cart-details.component';
+import { CheckoutPageComponent } from './cart/checkout-page/checkout-page.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    CartDetailsComponent,
+    CheckoutPageComponent
   ],
   imports: [
     BrowserModule
